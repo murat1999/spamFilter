a2e2b52e15958e2d0b6c7bcdd0bfa15c388e0dcd
murat1999 <muratbaktur@gmail.com>
2022-10-21 13:38:29 +0600

    spam detection

--- main.py
+++ main.py
@@ -4,28 +4,60 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import re
 
-def isSpam(content, spamLinkDomains = [], redirectionDepth = 1):
-    # myString = "This is my tweet check it out https://example.com/blah/fsfsfs"
+def checkLink(response, spamLinkDomains):
+    domainList = []
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = soup.find_all('a')
+
+    for link in links:
+        domain = urlparse(link.get('href')).netloc
+        if domain:
+            domainList.append(domain)
+    for spamLink in spamLinkDomains:
+        if spamLink in domainList:
+            print(spamLink, 'Spam link detected')
+            return True
+
+def isSpam(content, spamLinkDomains, redirectionDepth):
     url = re.search("(?P<url>https?://[^\s]+)", content).group("url")
 
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links = soup.find_all('a')
-    for link in soup.find_all('a'):
-        if "moimstg.page.link" in link.get('href'):
-            print(link.get('href'))
-    # print(links)
+    if urlparse(url).netloc in spamLinkDomains:
+        print('YESS')
+        return True
 
+    redirectUrls = []
+    for url in response.history[1:]:
+        redirectUrls.append(url.url)
+    redirectUrls.append(response.url)
+
+    print(redirectUrls)
 
     if response.history:
         print("Request was redirected")
+
+        # if redirectionDepth <= len(redirectUrls):
+        for i in range(redirectionDepth):
+            if i + 1 <= len(redirectUrls):
+                checkingUrl = urlparse(redirectUrls[i]).netloc
+                if checkingUrl in spamLinkDomains:
+                    print('MURAT')
+                    return True
+                # subResponce = requests.get(redirectUrls[i])
+                # checkLink(subResponce, spamLinkDomains)
+            else:
+                # for j in range(i + 1, redirectionDepth):
+                checkLink(requests.get(redirectUrls[-1]), spamLinkDomains)
+
+
         for resp in response.history:
             print(resp.status_code, resp.url)
         print("Final destination:")
         print(response.status_code, response.url)
+        # print(False)
     else:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        links = soup.find_all('a.href')
+        checkLink(response, spamLinkDomains)
+    return False
 
 if __name__ == "__main__":
-    isSpam('https://moimstg.page.link/dmCn')
+    isSpam('spam spam https://moimstg.page.link/dmCn', ['skills.github.com'], 1)
